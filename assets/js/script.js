5fdd1b84efccb00690497cd1494ef484058cc618
kcode <karapoole13@gmail.com>
2024-08-03 19:00:37 -0500

    comments

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// retrieve user input for city
+// store searched cities at buttons
+// get lat and longitue from api call using city from user input
+// use latitute and longitute to get weather forcast
+// display the forcast back onto index.html
+//
